eae2a0526d7d4ce4be3529ada6d0edeb59228b41
Maud Gautier <maud.j.gautier@gmail.com>
2024-02-26 22:31:38 -0500

    feat: working version to render a login page

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
 import express from 'express'
+import path from 'path';
+
 const app = express()
 const port = 5001
+
+
+// Set the directory where the views are located
+app.set('views', path.join(__dirname, '/../views'));
+app.use('/static', express.static('public'))
+
+// Set EJS as the view engine but use it to render .html files
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
+// ---- LOGIN -----
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
 app.get('/', (_, res) => {
-  res.status(200).send()
+    res.redirect("/login")
 })
 app.listen(port, () => console.log(`Running on port ${port}`))
-console.log("in there !")
+
+
