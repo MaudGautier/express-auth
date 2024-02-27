d667743a33d5b11ab40c06d8745f28402722adfe
Maud Gautier <maud.j.gautier@gmail.com>
2024-02-26 22:54:44 -0500

    feat: make post requests when submitting the form

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
 import path from 'path';
 
 const app = express()
 const port = 5001
+
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 
 // Set the directory where the views are located
@@ -13,13 +17,33 @@ app.use('/static', express.static('public'))
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+type UserPassword = { password: string }
+type User = Record<number, UserPassword>
+
+// In-memory DB
+const DATABASE = {
+    users: {} as User[],
+    getUser(id: number) {
+        return DATABASE.users[id]
+    },
+    setUser(id: number, password: UserPassword) {
+        DATABASE.users[id] = password
+        return password
+    }
+}
+
 
 // ---- LOGIN -----
-app.get("/login", (req, res) => {
+app.get("/login", (_: Request, res: Response) => {
     res.render("login")
 })
 
-app.get('/', (_, res) => {
+app.post("/login", (req: Request, res: Response) => {
+    console.log("body", req.body)
+
+})
+
+app.get('/', (_: Request, res: Response) => {
     res.redirect("/login")
 })
 app.listen(port, () => console.log(`Running on port ${port}`))
